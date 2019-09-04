2a8f5777ef02dcb3faf9bc3b17d360c2664b2f2e
Martin Hartman <hartmanx1@wp.pl>
2019-09-04 09:15:53 +0200

    25.6 done

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+
+app.get('/', function (req, res) {
+    res.render('login');
+});
+
+app.get('/auth/google', function (req, res)  {
+    res.render('logged')
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+
+app.listen(3000);
